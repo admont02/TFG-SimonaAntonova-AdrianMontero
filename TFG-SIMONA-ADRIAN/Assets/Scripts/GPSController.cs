0d4f61b6fba8551d75f72c8319e3eb8ab6b928c6
admont02 <admont02@ucm.es>
2025-03-05 02:40:13 +0100

    GPS mejorado (esperemos jeje)

--- TFG-SIMONA-ADRIAN/Assets/Scripts/GPSController.cs
+++ TFG-SIMONA-ADRIAN/Assets/Scripts/GPSController.cs
@@ -97,6 +97,115 @@ public class GPSController : MonoBehaviour
         }
     }
 
+    //private void CreatePathRenderer()
+    //{
+    //    if (pathRendererObject != null)
+    //    {
+    //        Destroy(pathRendererObject);
+    //    }
+
+    //    pathRendererObject = Instantiate(pathRendererPrefab);
+    //    lineRenderer = pathRendererObject.GetComponent<LineRenderer>();
+
+
+    //    int i = 0;
+    //    for (int j = 0; j < path.Count; j++)
+    //    {
+    //        //path[j]
+    //        if (posicionesPiezas[path[j].Id].GetComponent<WaypointContainer>().GetWaypoints() != null)
+    //            posicionesPiezas[path[j].Id].GetComponent<WaypointContainer>().Calculate();
+
+
+    //        var currentWaypointContainer = posicionesPiezas[path[j].Id].GetComponent<WaypointContainer>();
+    //        var waypoints = currentWaypointContainer.GetWaypoints();
+    //        Waypoint aux = waypoints.GetComponent<Waypoint>();
+
+    //        while (aux.next != null)
+    //        {
+    //            if (aux.branches.Count > 0)
+    //            {
+    //                if (j + 1 <= path.Count)
+    //                    if (CheckWaypointDistance(aux.branches[0], aux.next, posicionesPiezas[path[j + 1].Id].transform.position))
+    //                    {
+
+    //                        Waypoint correctBranch = GetCorrectBranch(aux);
+
+    //                        if (correctBranch != null)
+    //                        {
+
+    //                            lineRenderer.SetPosition(i, correctBranch.transform.position);
+    //                            i++;
+
+
+    //                            var nextWaypoint = correctBranch.next;
+    //                            while (nextWaypoint != null)
+    //                            {
+    //                                lineRenderer.SetPosition(i, nextWaypoint.transform.position);
+    //                                i++;
+    //                                nextWaypoint = nextWaypoint.next;
+    //                            }
+    //                            break;
+    //                        }
+    //                    }
+    //                    else
+    //                    {
+    //                        lineRenderer.SetPosition(i, aux.GetPosition());
+    //                        i++;
+    //                    }
+    //            }
+    //            else
+    //            {
+    //                lineRenderer.SetPosition(i, aux.GetPosition());
+    //                i++;
+    //            }
+    //            if (aux.next != null)
+    //            {
+    //                aux = aux.next;
+    //                aux.previous.next = null;
+    //            }
+    //        }
+    //    }
+    //}
+    private bool CheckWaypointDistance(Waypoint a, Waypoint b, Vector3 pos)
+    {
+        return (Vector3.Distance(a.GetPosition(), pos) < Vector3.Distance(a.GetPosition(), pos));
+
+
+    }
+
+    //Método para obtener la rama correcta hacia el destino
+    //private Waypoint GetCorrectBranch(Waypoint waypoint)
+    //{
+
+    //    Node goalNode = new Node(targetNodeId);
+    //    Vector3 destinationPosition = posicionesPiezas[goalNode.Id].transform.position;
+
+
+    //    Vector3 directionToDestination = destinationPosition - waypoint.transform.position;
+
+
+    //    Waypoint correctBranch = null;
+    //    float maxDotProduct = float.MinValue;
+
+
+    //    foreach (var branch in waypoint.branches)
+    //    {
+
+    //        Vector3 directionToBranch = branch.transform.position - waypoint.transform.position;
+
+
+    //        float dotProduct = Vector3.Dot(directionToDestination.normalized, directionToBranch.normalized);
+
+
+    //        if (dotProduct > maxDotProduct)
+    //        {
+    //            maxDotProduct = dotProduct;
+    //            correctBranch = branch;
+    //        }
+    //    }
+
+    //    return correctBranch;
+    //}
     private void CreatePathRenderer()
     {
         if (pathRendererObject != null)
@@ -107,110 +216,78 @@ public class GPSController : MonoBehaviour
         pathRendererObject = Instantiate(pathRendererPrefab);
         lineRenderer = pathRendererObject.GetComponent<LineRenderer>();
 
+        List<Vector3> linePositions = new List<Vector3>();
 
-        int i = 0;
-        for (int j = 0; j < path.Count; j++)
+        for (int i = 0; i < path.Count - 1; i++)
         {
-            //path[j]
-            if (posicionesPiezas[path[j].Id].GetComponent<WaypointContainer>().GetWaypoints() != null)
-                posicionesPiezas[path[j].Id].GetComponent<WaypointContainer>().Calculate();
-
-
-            var currentWaypointContainer = posicionesPiezas[path[j].Id].GetComponent<WaypointContainer>();
-            var waypoints = currentWaypointContainer.GetWaypoints();
-            Waypoint aux = waypoints.GetComponent<Waypoint>();
-
-            while (aux.next != null)
+            WaypointContainer currentWaypointContainer = posicionesPiezas[path[i].Id].GetComponent<WaypointContainer>();
+            if (currentWaypointContainer.GetWaypoints() == null)
             {
-                if (aux.branches.Count > 0)
+                currentWaypointContainer.Calculate();
+            }
+
+            GameObject waypointObject = currentWaypointContainer.GetWaypoints();
+            Waypoint waypointComponent = waypointObject.GetComponent<Waypoint>();
+
+            if (waypointComponent == null)
+            {
+                continue; // Saltar si no hay waypoints
+            }
+
+            linePositions.Add(waypointComponent.transform.position);
+
+            while (waypointComponent != null)
+            {
+                if (waypointComponent.branches.Count > 0)
                 {
-                    if (j + 1 <= path.Count)
-                        if (CheckWaypointDistance(aux.branches[0], aux.next, posicionesPiezas[path[j + 1].Id].transform.position))
-                        {
-
-                            Waypoint correctBranch = GetCorrectBranch(aux);
-
-                            if (correctBranch != null)
-                            {
-
-                                lineRenderer.SetPosition(i, correctBranch.transform.position);
-                                i++;
-
-
-                                var nextWaypoint = correctBranch.next;
-                                while (nextWaypoint != null)
-                                {
-                                    lineRenderer.SetPosition(i, nextWaypoint.transform.position);
-                                    i++;
-                                    nextWaypoint = nextWaypoint.next;
-                                }
-                                break;
-                            }
-                        }
-                        else
-                        {
-                            lineRenderer.SetPosition(i, aux.GetPosition());
-                            i++;
-                        }
+                    Waypoint correctBranch = GetCorrectBranch(waypointComponent, posicionesPiezas[path[i + 1].Id].transform.position);
+                    if (correctBranch != null)
+                    {
+                        waypointComponent = correctBranch;
+                    }
+                    else
+                    {
+                        waypointComponent = waypointComponent.next;
+                    }
                 }
                 else
                 {
-                    lineRenderer.SetPosition(i, aux.GetPosition());
-                    i++;
+                    waypointComponent = waypointComponent.next;
                 }
-                if (aux.next != null)
+
+                if (waypointComponent != null)
                 {
-                    aux = aux.next;
-                    aux.previous.next = null;
+                    linePositions.Add(waypointComponent.transform.position);
                 }
             }
         }
+
+        // Adding the final target position
+        linePositions.Add(posicionesPiezas[targetNodeId].transform.position);
+
+        lineRenderer.positionCount = linePositions.Count;
+        lineRenderer.SetPositions(linePositions.ToArray());
     }
-    private bool CheckWaypointDistance(Waypoint a, Waypoint b, Vector3 pos)
+
+    private Waypoint GetCorrectBranch(Waypoint waypoint, Vector3 destinationPosition)
     {
-        return (Vector3.Distance(a.GetPosition(), pos) < Vector3.Distance(a.GetPosition(), pos));
-
-
-    }
-
-    //Método para obtener la rama correcta hacia el destino
-    private Waypoint GetCorrectBranch(Waypoint waypoint)
-    {
-
-        Node goalNode = new Node(targetNodeId);
-        Vector3 destinationPosition = posicionesPiezas[goalNode.Id].transform.position;
-
-
-        Vector3 directionToDestination = destinationPosition - waypoint.transform.position;
-
-
         Waypoint correctBranch = null;
-        float maxDotProduct = float.MinValue;
-
+        float minDistance = float.MaxValue;
+        if (waypoint.next != null)
+            minDistance = Vector3.Distance(waypoint.next.transform.position, destinationPosition);
 
         foreach (var branch in waypoint.branches)
         {
-
-            Vector3 directionToBranch = branch.transform.position - waypoint.transform.position;
-
-
-            float dotProduct = Vector3.Dot(directionToDestination.normalized, directionToBranch.normalized);
-
-
-            if (dotProduct > maxDotProduct)
+            float distance = Vector3.Distance(branch.transform.position, destinationPosition);
+            if (distance < minDistance)
             {
-                maxDotProduct = dotProduct;
+                minDistance = distance;
                 correctBranch = branch;
             }
         }
 
         return correctBranch;
     }
-
-
-
-
-
 
     private void RecalculatePath()
     {
@@ -249,7 +326,7 @@ public class GPSController : MonoBehaviour
 
         if (timeSinceLastRecalculation >= recalculationInterval)
         {
-            //RecalculatePath();
+            RecalculatePath();
             timeSinceLastRecalculation = 0.0f;
         }
 
