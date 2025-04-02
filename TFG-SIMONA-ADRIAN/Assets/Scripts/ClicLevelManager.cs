afbe001e7483601badf7f1577873c927fdbb11a7
admont02 <admont02@ucm.es>
2025-04-02 21:07:21 +0200

    cochesIA paran al acabar un nivel de clicl

--- TFG-SIMONA-ADRIAN/Assets/Scripts/ClicLevelManager.cs
+++ TFG-SIMONA-ADRIAN/Assets/Scripts/ClicLevelManager.cs
@@ -90,6 +90,14 @@ public class ClicLevelManager : MonoBehaviour
         else
             yield return StartCoroutine(MoveCarsInDisorder(priorityCarList));
 
+        foreach (var car in priorityCarList)
+        {
+            OtherCar otherCar = car.GetComponent<OtherCar>();
+            if (otherCar != null)
+            {
+                otherCar.clickMove = false; 
+            }
+        }
         GameManager.Instance.ComprobarNivel();
     }
 
@@ -102,7 +110,7 @@ public class ClicLevelManager : MonoBehaviour
             {
                 otherCar.arrow.SetActive(false);
                 otherCar.clickMove = true;
-                yield return new WaitForSeconds(3.0f);
+                yield return new WaitForSeconds(5.0f);
                 
             }
         }
@@ -119,6 +127,7 @@ public class ClicLevelManager : MonoBehaviour
 
             }
         }
-        yield return new WaitForSeconds(8.0f);
+        yield return new WaitForSeconds(3.0f);
+
     }
 }
